9c224ef9a21710a0f9710aa9148d670e27626933
charlescrg <76757312+charlescrg@users.noreply.github.com>
2025-08-13 15:25:38 -0300

    Update SimulacaoResource.java

--- src/main/java/gov/caixa/simuladorservice/resource/SimulacaoResource.java
+++ src/main/java/gov/caixa/simuladorservice/resource/SimulacaoResource.java
@@ -14,7 +14,7 @@ import jakarta.ws.rs.core.Response;
 import org.eclipse.microprofile.openapi.annotations.Operation;
 import org.eclipse.microprofile.openapi.annotations.tags.Tag;
 
-@Path("/simulacao")
+@Path("/api/simulacao")
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
 @Tag(name = "Simulação", description = "Endpoint para simulação de empréstimos")
