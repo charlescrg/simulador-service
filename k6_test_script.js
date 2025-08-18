ec28f8171feb9d4b5a5a1dd4f98262b75aaee920
charlescrg <76757312+charlescrg@users.noreply.github.com>
2025-08-18 14:03:01 -0300

    Create k6_test_script.js

---
+++ k6_test_script.js
@@ -0,0 +1,19 @@
+import http from 'k6/http';
+import { check, sleep } from 'k6';
+
+export let options = {
+    stages: [
+        { duration: '30s', target: 10 }, 
+        { duration: '1m', target: 10 },  
+        { duration: '30s', target: 0 },  
+    ],
+};
+
+export default function () {
+    let res = http.get('http://localhost:8080/telemetria');
+    check(res, {
+        'status is 200': (r) => r.status === 200,
+        'response time < 500ms': (r) => r.timings.duration < 500,
+    });
+    sleep(1);
+}
