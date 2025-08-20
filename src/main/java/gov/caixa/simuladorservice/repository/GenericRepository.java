3b36c799b8fefcc2ae92024d60094946d147b140
charlescrg <76757312+charlescrg@users.noreply.github.com>
2025-08-20 18:51:47 -0300

    Update GenericRepository.java

--- src/main/java/gov/caixa/simuladorservice/repository/GenericRepository.java
+++ src/main/java/gov/caixa/simuladorservice/repository/GenericRepository.java
@@ -1,8 +1,10 @@
 package gov.caixa.simuladorservice.repository;
 
 
+import io.quarkus.agroal.DataSource;
 import io.quarkus.hibernate.orm.panache.PanacheRepositoryBase;
 
+@DataSource("local")
 public interface GenericRepository<T, ID> extends PanacheRepositoryBase<T, ID> {
     // Pode deixar vazio, pois já herda métodos do PanacheRepositoryBase
 }
