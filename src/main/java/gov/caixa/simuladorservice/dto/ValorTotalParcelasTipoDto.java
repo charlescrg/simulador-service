b1ac24bc46fab5ff8184e64a59e6b85ba99e555c
charlescrg <76757312+charlescrg@users.noreply.github.com>
2025-08-22 14:03:49 -0300

    Update ValorTotalParcelasTipoDto.java

--- src/main/java/gov/caixa/simuladorservice/dto/ValorTotalParcelasTipoDto.java
+++ src/main/java/gov/caixa/simuladorservice/dto/ValorTotalParcelasTipoDto.java
@@ -1,6 +1,8 @@
 package gov.caixa.simuladorservice.dto;
 
 import java.math.BigDecimal;
+
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.AllArgsConstructor;
@@ -8,8 +10,11 @@ import lombok.AllArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Schema(description = "DTO que representa o valor total das parcelas por tipo")
 public class ValorTotalParcelasTipoDto {
+    @Schema(description = "Tipo da parcela", example = "FINANCIAMENTO")
     private String tipo;
+
+    @Schema(description = "Valor total das parcelas", example = "15000.00")
     private BigDecimal valorTotalParcelas;
 }
-
